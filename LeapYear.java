a826b71890c366857b2c10934a2673d23f1ca2fd
vinod kumar <sirikondaviinod99@gmail.com>
2025-01-29 16:44:46 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,11 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int a=2005;
+        if(a%4==0){
+            System.out.println("leap year");
+
+        }else{
+            System.out.println("not a leap year");
+        }
+    }
+}
